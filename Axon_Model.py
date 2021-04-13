f6b249745f1722d5e6cc69aca5c73fad87bac07d
Luis Eduardo Sánchez <lsgm16308@gmail.com>
2021-04-12 19:35:53 -0500

    Primer programa

---
+++ Axon_Model.py
@@ -0,0 +1,73 @@
+"""
+Simulación de señales neuronales mediante circuitos RC 
+
+Concurso de aparatos y experimentos
+Luis Eduardo Sánchez González
+
+lun 12 abr 2021 19:23:12 CDT 
+
+Repositorio Github: 
+"""
+import numpy as np
+
+class Axon:
+    
+	def __init__(self, V0, C, R1, R2, N):
+        
+		self.C, self.R1, self.R2 = C, R1, R2  
+        
+		try:
+        
+			self.InitialConditions = np.zeros(N)
+        
+			if isinstance(V0, (int, float)):
+        
+				self.V0 = lambda t: V0  
+            
+			elif callable(V0):
+            
+				self.V0 = V0 
+                
+			self.InitialConditions[0] = self.V0(0)
+            
+		except:
+            
+			print("Voltaje no puede ser definido")
+    
+	def dV(self, Vi, Vf):
+        
+		return (1/self.C)*(((Vi-Vf)/self.R2) - (Vf/self.R1))
+
+	def __call__(self, v, t):
+        
+		V = np.zeros_like(v)
+		V[0] = self.dV(self.V0(t), v[0])
+
+		for i in range(len(v) - 1):
+            
+			V[i + 1] += self.dV(v[i], v[i + 1]) 
+
+		return V
+
+if __name__ == "__main__":
+
+	from sys import path
+	path.append("../")
+	import matplotlib.pyplot as plt
+	from PhysicsPy.ODEsMethods import *
+
+	Axon_0 = Axon(70e-3, 1e-10, 1e8, 1e6, N = 100)
+
+	Solucion = Euler(Axon_0)
+	Solucion.InitialConditions(Axon_0.InitialConditions, [0, 1e-2], 1e-6)
+	V, t = Solucion.SolveODE()
+
+	for i in range(0, len(t), 1000):
+
+    		plt.plot(V[i,:], label ="t ="+f"{round(i*1e-6,3)} s")
+
+	plt.title("Voltaje en los circuitos")
+	plt.grid() ; plt.legend() 
+	plt.ylabel("Voltaje (V)")
+	plt.xlabel("Circuito (N)")
+	plt.show()
